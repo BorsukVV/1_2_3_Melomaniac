884f8815abae612ad374c9f284ac8f35805a1390
Borsuk Vladimir <grifon26@inbox.ru>
2022-03-20 09:44:35 +0300

    fixed the logic of calculating premium discounts

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -2,32 +2,40 @@ package ru.netology
 
 fun main() {
 
-    val previosPeriodSpent = 12000_00
-    val price = 600_00
+    val price = 15000_00
     val stanardDiscount = 100_00
     val premiumDiscount = 5
     val melomaniacDiscount = 1
     val ifUserMelomaniac = true
-    val discountStart = 1_000_00
-    val premiumDscountStart = 10_000_00
+    val discountStart = 1_000_99
+    val premiumDscountStart = 10_000_99
     val totalPrice: Int
+    val totalPercentagesAsWhole = 100
 
-    println("За прошлый период вы потратили ${convertPriceToString(previosPeriodSpent)}")
     println("Вы совершаете покупку на ${convertPriceToString(price)}")
 
-    if (previosPeriodSpent < discountStart) {
-        val melomaniacDiscountSummary = if (ifUserMelomaniac) price * melomaniacDiscount / 100 else 0
-        totalPrice = price - melomaniacDiscountSummary
+    if (price < discountStart) {
+        totalPrice = if (ifUserMelomaniac) {
+            price * (totalPercentagesAsWhole - melomaniacDiscount) / totalPercentagesAsWhole
+        } else {
+            price
+        }
         println("Сумма платежа с учетом скидок составляет  ${convertPriceToString(totalPrice)}")
-    } else if (discountStart < previosPeriodSpent && previosPeriodSpent < premiumDscountStart) {
-        val standardDiscountSummary = price - stanardDiscount
-        val melomaniacDiscountSummary = if (ifUserMelomaniac) standardDiscountSummary * melomaniacDiscount / 100 else 0
-        totalPrice = standardDiscountSummary - melomaniacDiscountSummary
+    } else if (price in (discountStart + 1) until premiumDscountStart) {
+        val afterStandardDiscount = price - stanardDiscount
+        totalPrice = if (ifUserMelomaniac) {
+            afterStandardDiscount * (totalPercentagesAsWhole - melomaniacDiscount) / totalPercentagesAsWhole
+        } else {
+            afterStandardDiscount
+        }
         println("Сумма платежа с учетом скидок составляет  ${convertPriceToString(totalPrice)}")
-    } else if (previosPeriodSpent > premiumDscountStart) {
-        val premiumDiscountSummary = price * premiumDiscount / 100
-        val melomaniacDiscountSummary = if (ifUserMelomaniac) premiumDiscountSummary * melomaniacDiscount / 100 else 0
-        totalPrice = premiumDiscountSummary - melomaniacDiscountSummary
+    } else if (price > premiumDscountStart) {
+        val afterPremiumDiscount = price * (totalPercentagesAsWhole - premiumDiscount) / totalPercentagesAsWhole
+        totalPrice = if (ifUserMelomaniac) {
+            afterPremiumDiscount * (totalPercentagesAsWhole - melomaniacDiscount)/ totalPercentagesAsWhole
+        } else {
+            afterPremiumDiscount
+        }
         println("Сумма платежа с учетом скидок составляет  ${convertPriceToString(totalPrice)}")
     }
 }
